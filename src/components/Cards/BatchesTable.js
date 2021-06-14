3595b7b7b4762af504344f15db181e5da34ebd83
ShubhJ99 <shubhjani1999@gmail.com>
2021-06-14 10:58:53 +0530

    Optimized

--- src/components/Cards/BatchesTable.js
+++ src/components/Cards/BatchesTable.js
@@ -1,8 +1,54 @@
 import React from "react";
-import Table from "components/Cards/Table.js";
+import {useEffect, useState} from 'react';
+
+const API_HOST = "http://localhost:3001/batch-table";
+const INVENTORY_API_URL = `${API_HOST}`;
 
 const Tabs = ({ color }) => {
   const [openTab, setOpenTab] = React.useState(1);
+  const [data, setData] = useState([]);
+    const fetchInventory = () => {
+        fetch(`${INVENTORY_API_URL}`)
+            .then(res => res.json())
+            .then(json => setData(json));
+    }
+    
+    useEffect(() => {
+        fetchInventory();
+    }, []);
+
+    const Table = (a) => {
+      return (
+        <div>
+          <div className="flex justify-between">
+              <div className="text-2xl font-bold m-5 ml-2">Student Data</div>
+              <div className="text-md font-bold m-5 ml-2"><i className="fas fa-plus-circle mr-2"></i>Add Branch</div>
+          </div>            
+          <table className="rounded-t-lg m-5 w-5/6 mx-auto bg-gray-200 text-gray-800" align="center" >
+              <thead>
+              <tr className="text-left border-b-2 border-gray-300">
+                  <th className="px-4 py-3">Batch</th>
+                  <th className="px-4 py-3">Branch</th>
+                  <th className="px-4 py-3">Students</th>
+                  <th className="px-4 py-3">CVR</th>
+              </tr>
+              </thead>
+              <tbody>
+                  {
+                      data.map((item) => (
+                          <tr className="bg-gray-100 border-b border-gray-200">
+                              <td className="px-4 py-3">{item.id = a}</td>
+                              <td className="px-4 py-3">{item.branch}</td>
+                              <td className="px-4 py-3">{item.students}</td>
+                              <td className="px-4 py-3">{item.cvr}</td>
+                          </tr>
+                  ))}
+              </tbody>
+          </table>
+        </div>
+      )
+    }
+
   return (
     <>
       <div className="flex flex-wrap">
@@ -72,14 +118,14 @@ const Tabs = ({ color }) => {
           <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-xl rounded">
             <div className="px-4 py-5 flex-auto">
               <div className="tab-content tab-space">
-                <div className={openTab === 1 ? "block" : "hidden"} id="link1">
-                  <Table />
+                <div className={openTab === 1 ? "block" : "hidden"} id="link1">                
+                  {Table("A")}                
                 </div>
                 <div className={openTab === 2 ? "block" : "hidden"} id="link2">
-                  <Table />
+                  {Table("B")}  
                 </div>
                 <div className={openTab === 3 ? "block" : "hidden"} id="link3">
-                  <Table />
+                  {Table("C")}  
                 </div>
               </div>
             </div>
